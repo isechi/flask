28f47253454e13a1051dea9154c362c5d03b0aeb
Ise Chieko <kimagure_rere@yahoo.co.jp>
2020-06-09 21:22:37 +0900

    ヘッダーとフッターのテンプレ作成

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 
 @app.route("/test")
 def test():
-    return render_template("test.html")
+    name = "flask"
+    return render_template("test.html",name = name)
+
+@app.route("/greet/<text>")
+def hello(text):
+    return text + "さん、こんにちは"
 
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     # サーバーを起動するよ
     app.run(debug=True)
     # デバッグモードを有効にするよ（使いたくない場合はFalse)
